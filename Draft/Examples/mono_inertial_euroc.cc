c0529d782cced9b73c5e0e4090549c571e5c969c
AZen <418130444@qq.com>
2023-10-12 12:42:14 +0800

    update

---
+++ Draft/Examples/mono_inertial_euroc.cc
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+
+    if(argc < 5)
+    {
+        cerr << endl << "Usage: ./mono_inertial_euroc path_to_vocabulary path_to_settings path_to_sequence_folder_1 path_to_times_file_1 (path_to_image_folder_2 path_to_times_file_2 ... path_to_image_folder_N path_to_times_file_N) " << endl;
+        return 1;
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+

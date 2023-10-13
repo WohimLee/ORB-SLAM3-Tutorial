7073487d05f489354050defcd9beb1795cd83a82
AZen <418130444@qq.com>
2023-10-13 14:55:07 +0800

    update

---
+++ 2.Implementation/0.Prerequisites/2.SerializationUtils/mks/Sophus.mk
@@ -0,0 +1,8 @@
+
+ROOT := /home/nerf/datav/SLAM/ORB_SLAM3/Thirdparty/Sophus
+
+Sophus_INCLUDE := $(ROOT)
+Sophus_LIB_DIR := 
+Sophus_LIBS := $(wildcard $(Sophus_LIB_DIR)/*.a $(Sophus_LIB_DIR)/*.so)
+Sophus_LIBS := $(basename $(notdir $(Sophus_LIBS)))
+Sophus_LIBS := $(patsubst lib%,%,$(Sophus_LIBS))

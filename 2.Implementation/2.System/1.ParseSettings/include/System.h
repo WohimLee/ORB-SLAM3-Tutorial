6622f48d9815a5273e6b293c25446a955f203055
AZen <418130444@qq.com>
2023-10-15 14:22:15 +0800

    update

---
+++ 2.Implementation/2.System/1.ParseSettings/include/System.h
@@ -0,0 +1,113 @@
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#include<thread>
+#include<opencv2/core/core.hpp>
+
+#include "Tracking.h"
+#include "FrameDrawer.h"
+#include "MapDrawer.h"
+#include "Atlas.h"
+#include "LocalMapping.h"
+#include "LoopClosing.h"
+#include "KeyFrameDatabase.h"
+#include "ORBVocabulary.h"
+#include "Viewer.h"
+#include "ImuTypes.h"
+#include "Settings.h"
+
+// 增加
+#include <iostream>
+#include <Eigen/Core>
+using namespace std;
+
+
+namespace ORB_SLAM3
+{
+
+class Verbose
+{
+public:
+    enum eLevel
+    {
+        VERBOSITY_QUIET=0,
+        VERBOSITY_NORMAL=1,
+        VERBOSITY_VERBOSE=2,
+        VERBOSITY_VERY_VERBOSE=3,
+        VERBOSITY_DEBUG=4
+    };
+
+    static eLevel th;
+
+public:
+    static void PrintMess(std::string str, eLevel lev)
+    {
+        if(lev <= th)
+            cout << str << endl;
+    }
+
+    static void SetTh(eLevel _th)
+    {
+        th = _th;
+    }
+};
+
+class Viewer;
+class FrameDrawer;
+class MapDrawer;
+class Atlas;
+class Tracking;
+class LocalMapping;
+class LoopClosing;
+class Settings;
+
+class System
+{
+public:
+    // Input sensor
+    enum eSensor{
+        MONOCULAR=0,
+        STEREO=1,
+        RGBD=2,
+        IMU_MONOCULAR=3,
+        IMU_STEREO=4,
+        IMU_RGBD=5,
+    };
+
+    // File type
+    enum FileType{
+        TEXT_FILE=0,
+        BINARY_FILE=1,
+    };
+
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    // Initialize the SLAM system. It launches the Local Mapping, Loop Closing and Viewer threads.
+    System(const string &strVocFile, const string &strSettingsFile, const eSensor sensor, const bool bUseViewer = true, const int initFr = 0, const string &strSequence = std::string());
+
+
+private:
+    eSensor mSensor;
+    Viewer* mpViewer;
+
+    // Reset flag
+    bool mbReset;
+    bool mbResetActiveMap;
+
+    // Change mode flags
+    bool mbActivateLocalizationMode;
+    bool mbDeactivateLocalizationMode;
+
+    // Shutdown flag
+    bool mbShutDown;
+};
+}
+
+
+#endif // SYSTEM_H

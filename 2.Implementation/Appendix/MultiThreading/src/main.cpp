fff20a783638bd5d1373938a7fe8f64ebcb4f58b
AZen <418130444@qq.com>
2023-10-18 00:36:20 +0800

    update

--- 2.Implementation/Appendix/MultiThreading/src/main.cpp
+++ 2.Implementation/Appendix/MultiThreading/src/main.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
 
-void func(int &a, int b){
-    b = a;
-    printf("Runing thread: 1\n");
+std::mutex mtx;
+std::condition_variable cv;
+std::queue<int> data_queue;
+bool finished = false;
+
+void producer(int n) {
+    for (int i = 0; i < n; ++i) {
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            data_queue.push(i);
+        } // lock is released here
+        
+        cv.notify_one(); // Notify the consumer
+    }
+
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        finished = true;
+    }
+    cv.notify_one();
+}
+
+void consumer() {
+    while (true) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{ return !data_queue.empty() || finished; }); // Wait until there's data to process or producer is finished
+
+        while (!data_queue.empty()) {
+            int data = data_queue.front();
+            data_queue.pop();
+            lock.unlock();  // Manually unlock to allow other operations while we're processing data
+
+            std::cout << "Processed: " << data << std::endl;
+
+            lock.lock();  // Manually relock for the next iteration
+        }
+
+        if (finished) {
+            break;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    std::thread prod(producer, 10);
+    std::thread cons(consumer);
+
+    prod.join();
+    cons.join();
+
+    return 0;
 }
 
 
-int main(int argc, char** argv)
-{
-    printf("Running Main Thread...\n");
-    int a = 888;
-    int b = 999;
-    std::thread t(func, std::ref(a), b);
-
-    t.join();
-
-    return  0;
-}
--- 2.Implementation/Appendix/MultiThreading/std::unique_lock.md
+++
@@ -1,3 +0,0 @@
-&emsp;
-# unique_lock
-
